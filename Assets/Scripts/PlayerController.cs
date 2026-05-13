4c0eeb7c2b9c0734d252be2afbf45bf325c6e7dc
Tiền Minh Dương <tmd31052005@gmail.com>
2026-05-13 18:57:36 +0700

    Level completion

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,33 @@ using DG.Tweening;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] bool lockMoving = false;
-    
+
+    private Sequence moveSequence;
+
+    void OnEnable()
+    {
+        WinPoint.OnLevelComplete += CompleteLevel;
+    }
+
+    void OnDisable()
+    {
+        WinPoint.OnLevelComplete -= CompleteLevel;
+    }
+
+    private void CompleteLevel()
+    {
+        lockMoving = true;
+        moveSequence?.Kill();
+
+        PlayerProgress.UnlockNextLevel();
+        SceneController.Instance.TransitionToScene($"Level {PlayerProgress.CurrentLevel}");
+    }
+
+    private void UnlockMoving()
+    {
+        lockMoving = false;
+    }
+
     public void OnJump()
     {
         Debug.Log("Jump!");
@@ -29,14 +55,14 @@ public class PlayerController : MonoBehaviour
     public void MoveWithPath(Path path)
     {
         lockMoving = true;
-        var sequence = DOTween.Sequence();
+        moveSequence = DOTween.Sequence();
         var currentPos = transform.position;
         foreach (var dir in path.directions)
         {
-            sequence.Append(transform.DOMove(currentPos + new Vector3(dir.x, dir.y, 0) * path.stepLength, 0.1f)
+            moveSequence.Append(transform.DOMove(currentPos + new Vector3(dir.x, dir.y, 0) * path.stepLength, 0.1f)
                 .SetEase(Ease.Linear));
             currentPos += new Vector3(dir.x, dir.y, 0) * path.stepLength;
         }
-        sequence.OnComplete(() => lockMoving = false);
+        moveSequence.OnComplete(() => lockMoving = false);
     }
 }
